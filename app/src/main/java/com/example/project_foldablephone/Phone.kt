f25884ac6652b263e99dfd73eedb01f4ed1041f7
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-12 21:18:01 -0400

    Se agrega contenido a clase Phone.

--- app/src/main/java/com/example/project_foldablephone/Phone.kt
+++ app/src/main/java/com/example/project_foldablephone/Phone.kt
@@ -1,4 +1,20 @@
 package com.example.project_foldablephone
 
-class Phone {
+open class Phone(var isScreenLightOn: Boolean = false) {
+    //Encender telefono
+    fun switchOn() {
+        //Pantalla encendida
+        isScreenLightOn = true
+    }
+    //Apagar telefono
+    fun switchOff() {
+        //Pantalla apgada
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
 }
+
